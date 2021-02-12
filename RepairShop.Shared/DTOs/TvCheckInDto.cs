b0e9362e4a67852cb26a1477918646aa5f0bb1e1
Ahmed Abu Shamla <thekingneo@gmail.com>
2021-02-12 12:39:13 +0200

    list customer's details

--- RepairShop.Shared/DTOs/TvCheckInDto.cs
+++ RepairShop.Shared/DTOs/TvCheckInDto.cs
@@ -4,6 +4,7 @@ namespace RepairShop.Shared.DTOs
 {
     public class TvCheckInDto
     {
+        public int Id { get; set; }
         public DateTime DateIn { get; set; }
         public string Failure { get; set; }
         public bool Returned { get; set; }
